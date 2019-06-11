adb12831ea41432b673f7e63ad05e8162d58497a
Denys Plietnikov <givisimo@gmail.com>
2019-06-11 23:49:45 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+const isLoginValid = function(login) {
+  return (4 <= login.length && login.length <= 16);
+};
+
+const isLoginUnique = function(allLogins, login) {
+   return allLogins.includes(login);
+};
+
+const addLogin = function(allLogins, login) {
+if (!isLoginValid(login)) {
+    console.log('Ошибка! Логин должен быть от 4 до 16 символов');
+  } else if (isLoginUnique(allLogins, login)) {
+  console.log('Такой логин уже используется!');
+ } else {
+      logins.push(login);
+    console.log('Логин успешно добавлен!');
+ }
+};
+//
+//
+// // Вызовы функции для проверки
+addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
+addLogin(logins, 'robotGoogles');// 'Такой логин уже используется!'
+addLogin(logins, 'Zod');// 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin(logins, 'jqueryisextremelyfast');// 'Ошибка! Логин должен быть от 4 до 16 символов'
